d8624d87588656d58ab998d5d6c603c8c65719b6
Richard Peterson <webbuyrich@gmail.com>
2016-01-17 15:12:25 -0600

    added buttons to trigger requests

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
 
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(){
-		if(xhr.readyState === 4){
-			document.getElementById('ajax').innerHTML = xhr.responseText;
-		}
-	};
-	xhr.open('GET', 'cover-page.html');
-	xhr.send();
+
+	
 
 });
+
+/*-------------------MAKING THE FIRST AJAX CALL --------------------*/
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+	if(xhr.readyState === 4){
+		document.getElementById('ajax').innerHTML = xhr.responseText;
+	}
+};
+xhr.open('GET', 'cover-page.html');
+function sendAjax(){
+	xhr.send();
+	document.getElementById('call-ajax').style.display = 'none';
+};
+
+//Go Back
+function goBack(){
+	window.open('index.html', '_self', false);
+}
+
